afa0be2557f4328346e10a5fed53d179134ae7fe
Acer <someplace@gmail.com>
2021-12-22 15:07:00 +0700

    project

--- main/views.py
+++ main/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/home.html')
+    if request.method == 'POST':
+        masukr = int(request.POST['r'])
+        masukt = int(request.POST['t'])
+        keluar = (1/3)*(22/7)*(masukr^2)*masukt
+        context = {'keluar': keluar,
+                   }
+        return render(request, 'main/home.html', context)
+
+    else:
+        return render(request, 'main/home.html', {})
